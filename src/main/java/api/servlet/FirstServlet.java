b0a7662ebd69020af332f17fdad37b6db84f8256
Yaroslav Agapov <agapovi56@gmail.com>
2022-06-17 15:01:35 +0500

    Initial commit

---
+++ src/main/java/api/servlet/FirstServlet.java
@@ -0,0 +1,23 @@
+/**
+ * @author @Euraks 17.06.2022
+ */
+package api.servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet("/index")
+public class FirstServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html");
+        PrintWriter printWriter = resp.getWriter();
+        printWriter.write("Hello");
+        printWriter.close();
+    }
+}
