2ddc3d4f60b5233a1c51952928c89463960d9216
Yaroslav Agapov <agapovi56@gmail.com>
2022-06-18 14:11:54 +0500

    Add page to input

--- src/main/java/api/servlet/FirstServlet.java
+++ src/main/java/api/servlet/FirstServlet.java
@@ -3,6 +3,7 @@
  */
 package api.servlet;
 
+
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -22,8 +23,30 @@ public class FirstServlet extends HttpServlet {
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
         resp.setContentType("text/html");
         try (PrintWriter printWriter = resp.getWriter()) {
-            string = req.getParameter("id");
-            printWriter.write("Parameter Id = " + string + " /");
+            printWriter.write("Parameter Id = " + req.getParameter("id") + " /");
+            printWriter.println();
+            printWriter.write("Name " + req.getParameter("name") + " age = " + req.getParameter("age"));
+        }
+    }
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws  IOException {
+
+        response.setContentType("text/html");
+
+        try (PrintWriter writer = response.getWriter()) {
+            String name = request.getParameter("username");
+            String age = request.getParameter("userage");
+            String gender = request.getParameter("gender");
+            String country = request.getParameter("country");
+            String[] courses = request.getParameterValues("courses");
+            writer.println("<p>Name: " + name + "</p>");
+            writer.println("<p>Age: " + age + "</p>");
+            writer.println("<p>Gender: " + gender + "</p>");
+            writer.println("<p>Country: " + country + "</p>");
+            writer.println("<h4>Courses</h4>");
+            for (String course : courses)
+                writer.println("<li>" + course + "</li>");
         }
     }
 }
